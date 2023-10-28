5672a704a158f4909362e9c62ecf740f914467f3
VUYANI NGCOBO <109946638+DXgriz@users.noreply.github.com>
2023-10-28 06:38:25 +0200

    Add files via upload

---
+++ src/java/com/cyber/defns/entities/Player.java
@@ -0,0 +1,59 @@
+package com.cyber.defns.entities;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author vuyan
+ */
+@Entity
+public class Player extends User implements Serializable {
+
+    
+    private String username;
+    private String password;
+    
+    @OneToOne
+    private GameStats progress;
+    
+    @OneToMany
+    private List<Course> courses;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public GameStats getProgress() {
+        return progress;
+    }
+
+    public void setProgress(GameStats progress) {
+        this.progress = progress;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+    
+}
